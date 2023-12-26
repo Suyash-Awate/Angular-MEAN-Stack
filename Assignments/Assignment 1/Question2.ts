04dfec6b5cdac61f523764e0f4ac5256a8ca205d
Suyash Sadashiv Awate <suyashsadashivawate@gmail.com>
2023-12-26 21:52:54 +0530

    Add files via upload

---
+++ Assignments/Assignment 1/Question2.ts
@@ -0,0 +1,26 @@
+/*
+Q.2. Write a typescript program which contains one function named as Area. That function should 
+calculate area of circle. Accept value of radius from user and return its area. Default value of PI 
+should be 3.14 if it is not provided by the caller.
+
+Input : 5
+Output : Area of circle is 78.5
+*/
+
+import * as promptSync from 'prompt-sync'
+const prompt = promptSync();
+
+function Area(radius : number) : number
+{
+    var PI : number = 3.14;
+    var Area : number = 0;
+
+    Area = PI*(radius*radius);
+
+    return Area;
+}
+
+const Radius = prompt('Enter the radius');
+
+var Result = Area(Radius);
+console.log("Area of circle is "+Result);
